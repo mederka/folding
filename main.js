4fd3817ab7d56c95bf898507f31eb7802ed389be
Meder Kamalov <meder@kamalov.net>
2019-03-14 10:41:15 +0100

    Toggle ngl representation

--- main.js
+++ main.js
@@ -2,25 +2,29 @@ document.addEventListener("DOMContentLoaded", function () {
   loadAllNGL();
 });
 
-function loadNGL(id) {
+function loadNGL(id, rep) {
   let div = document.getElementById(id);
   let stage = new NGL.Stage(id);
   if (div.className.indexOf('spin') != -1) stage.setSpin(true);
   stage.setParameters({
     backgroundColor: "white"
   });
-  stage.loadFile(`rcsb://${id}`, {defaultRepresentation: true});
+  stage.loadFile(`rcsb://${id}`).then(out => {
+    out.addRepresentation(rep);
+    out.autoView();
+  });
 }
 
 function loadAllNGL() {
   const ngl = document.getElementsByClassName('ngl');
   if (ngl.length) {
     let id = ngl[0].id;
-    loadNGL(id);
+    loadNGL(id, 'cartoon');
   }
 }
 
 function openViewer(id) {
+  document.getElementById('toggleRep').value = 'cartoon';
   let modal = document.getElementById('viewer-modal');
   modal.style.display = 'block';
   let viewer = document.getElementById('ngl-viewer');
@@ -28,7 +32,19 @@ function openViewer(id) {
   stage.className = 'ngl ngl-big';
   stage.id = id;
   viewer.appendChild(stage);
-  loadNGL(id);
+  loadNGL(id, 'cartoon');
+}
+
+function toggleRep() {
+  const rep = document.getElementById('toggleRep').value;
+  const viewer = document.getElementById('ngl-viewer');
+  const id = document.getElementsByClassName('ngl')[0].id;
+  viewer.removeChild(document.getElementById(id));
+  let stage = document.createElement('div');
+  stage.className = 'ngl ngl-big';
+  stage.id = id;
+  viewer.appendChild(stage);
+  loadNGL(id, rep);
 }
 
 function closeViewer() {
