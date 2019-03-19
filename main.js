2b8d85926bc684931a5a7f1d267428ba20f9934f
Meder Kamalov <meder@kamalov.net>
2019-03-19 21:52:37 +0100

    Specific coloring for the helical peptide

--- main.js
+++ main.js
@@ -2,15 +2,16 @@ document.addEventListener("DOMContentLoaded", function () {
   loadAllNGL();
 });
 
-function loadNGL(id, rep) {
+function loadNGL(id, rep, color) {
   let div = document.getElementById(id);
   let stage = new NGL.Stage(id);
   if (div.className.indexOf('spin') != -1) stage.setSpin(true);
+  if (!color) color = 'hydrophobicity';
   stage.setParameters({
     backgroundColor: "white"
   });
   stage.loadFile(`rcsb://${id}`).then(out => {
-    out.addRepresentation(rep, { color: 'hydrophobicity' });
+    out.addRepresentation(rep, { color });
     out.autoView();
   });
 }
@@ -23,7 +24,7 @@ function loadAllNGL() {
   }
 }
 
-function openViewer(id) {
+function openViewer(id, color) {
   document.getElementById('toggleRep').value = 'cartoon';
   let modal = document.getElementById('viewer-modal');
   modal.style.display = 'block';
@@ -32,19 +33,21 @@ function openViewer(id) {
   stage.className = 'ngl ngl-big';
   stage.id = id;
   viewer.appendChild(stage);
-  loadNGL(id, 'cartoon');
+  loadNGL(id, 'cartoon', color);
 }
 
 function toggleRep() {
+  let color = 'hydrophobicity';
   const rep = document.getElementById('toggleRep').value;
   const viewer = document.getElementById('ngl-viewer');
   const id = document.getElementsByClassName('ngl')[0].id;
+  if (id == '1dn3') color = 'element';
   viewer.removeChild(document.getElementById(id));
   let stage = document.createElement('div');
   stage.className = 'ngl ngl-big';
   stage.id = id;
   viewer.appendChild(stage);
-  loadNGL(id, rep);
+  loadNGL(id, rep, color);
 }
 
 function closeViewer() {
